bc0195bc5462826ad10cdda10432888d1e105a66
Jessie Chen <jessiechen0513@gmail.com>
2018-01-13 17:02:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function nowServing(katzDeliLine){
 
 function currentLine(katzDeliLine){
   if (katzDeliLine.length >0){
-    return (`The line is currently: 1. ${katzDeliLine[0]} 2. ${katzDeliLine[1]} 3. ${katzDeliLine[2]})
+    return (`The line is currently: 1. ${katzDeliLine[0]} 2. ${katzDeliLine[1]} 3. ${katzDeliLine[2]}`);
   }
 }
